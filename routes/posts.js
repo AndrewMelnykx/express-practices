890eca75a380b657c9a07a34ab99baa071c5d460
Andrew Melnykx <andreymelnyk777@gmail.com>
2025-01-14 21:49:06 +0100

    Express practices introduction

--- routes/posts.js
+++ routes/posts.js
@@ -52,35 +52,36 @@ router.get("/:id", (req, res) => {
 //   { id: 4, title: "Title Four" },
 //   { id: 5, title: "Title Five" },
 // ];
-// //Get all posts
-// app.get("/api/posts", (req, res) => {
-//   const limit = parseInt(req.query.limit);
+//Get all posts
+app.get("/api/posts", (req, res) => {
+  const limit = parseInt(req.query.limit);
 
-//   if (!isNaN(limit) && limit > 0) {
-//     return res.json(posts.slice(0, limit));
-//   }
-//   // res.json(posts);
-//   res.status(200).json(post);
-// });
+  if (!isNaN(limit) && limit > 0) {
+    return res.json(posts.slice(0, limit));
+  }
+  // res.json(posts);
+  res.status(200).json(post);
+});
 
-// //Get single post
-// app.get("/api/posts/:id", (req, res) => {
-//   const id = parseInt(req.params.id);
-//   const post = posts.find((post) => post.id === id);
+//Get single post
+app.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
 
-//   if (!post) {
-//     return res
-//       .status(404)
-//       .json({ message: `A post with the id of ${id} wasnt found` });
-//   }
-//   res.status(200).json(post);
+  if (!post) {
+    return res
+      .status(404)
+      .json({ message: `A post with the id of ${id} wasnt found` });
+  }
+  res.status(200).json(post);
 
-//   //   res.json(posts.filter((post) => post.id === id));
-// });
+  //   res.json(posts.filter((post) => post.id === id));
+});
 
 //Create new post
 router.post("/", (req, res) => {
   console.log(req.body);
+
   res.status(201).json(posts);
 });
 
